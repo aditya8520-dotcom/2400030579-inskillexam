9b054d56fee83728fe96fe2a695bcabfaa2e8796
saiaditya <2400030579@kluniversity.in>
2025-10-16 10:40:15 +0530

    my skill exam

---
+++ fr/src/App.jsx
@@ -0,0 +1,14 @@
+import Welcome from "./Welcome.jsx";
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Deepak" />
+      <Welcome name="Aditya" />
+      <Welcome name="Riya" />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ fr/src/Components/Welcome.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+function Welcome(props) {
+  return (
+    <h2>Welcome, {props.name}!</h2>
+  );
+}
+
+export default Welcome;
